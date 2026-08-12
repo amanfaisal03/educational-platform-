483ef4769a7defbddbbd82e9cd9212cdc66f31a3
Aman Alobeytha <aman.alobeytha@joacademy.com>
2026-08-12 14:17:19 +0300

    add seed to create admin in databas

--- app/core/Config.py
+++ app/core/Config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     database_url: str
     # front_end_url: str
     secret_key: str = "0c4bf72e725e337a63dc87f8efe350ff8b21c8e1b53cf2520533ef957fcd6acb"
+    admin_name: str
+    admin_password: str
+    admin_email: str
+
 
     _project_root = Path(__file__).resolve().parent
     _env_file = _project_root / ".env"
---
+++ app/db/admin_seed.py
@@ -0,0 +1,31 @@
+from app.db.database import SessionLocal
+from sqlalchemy import select
+
+from app.core.Config import settings
+from app.core.security import hash_password
+from app.models.user import User
+
+def create_admin()->None:
+    with SessionLocal() as db:
+        existing_user=db.query(User).filter(User.email==settings.admin_email).first()
+        if existing_user:
+            if existing_user.role =="admin":
+                print("admin already exists")
+                return
+            raise RuntimeError(
+                "The admin email already belongs to a student."
+            )
+
+        admin=User(
+            name=settings.admin_name,
+            email=settings.admin_email,
+            password=hash_password(settings.admin_password),
+            role="admin",
+
+        )
+        db.add(admin)
+        db.commit()
+        print("admin created successfully ")
+
+if __name__ == "__main__":
+    create_admin()
