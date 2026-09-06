c6c1e73d9fd6732fe634b5f58109a0325bb24ee1
Aman Alobeytha <aman.alobeytha@joacademy.com>
2026-09-06 11:23:36 +0300

    update frontend page styling

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from api.routers.admin import admin_router
 from api.routers.auth import auth_router
@@ -10,6 +11,7 @@ app = FastAPI(
     description="API for managing courses, students, lessons, and authentication.",
     version="1.0.0",
 )
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(auth_router)
 app.include_router(courses_router)
 app.include_router(admin_router)
